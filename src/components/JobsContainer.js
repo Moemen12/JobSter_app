afcd264d6d3b13633dcb104b53c8ba910deb5f4a
moemen saadeh <moemensaadeh936@gmail.com>
2023-12-11 02:52:26 +0300

    jobster

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -22,7 +22,7 @@ const JobsContainer = () => {
 
   useEffect(() => {
     dispatch(getAllJobs());
-  }, [page, search, searchStatus, searchType, sort]);
+  }, [page, search, searchStatus, searchType, sort, dispatch]);
 
   if (isLoading) {
     return <Loading center />;
